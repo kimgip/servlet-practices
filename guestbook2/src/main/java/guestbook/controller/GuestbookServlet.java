d9635624f2a5f4f742a1257a5beadf68c1b7bba6
kimgip <gippeum0102@gmail.com>
2024-05-23 15:10:32 +0900

    model2 연습 - guestbook completed

---
+++ guestbook2/src/main/java/guestbook/controller/GuestbookServlet.java
@@ -0,0 +1,61 @@
+package guestbook.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import guestbook.dao.GuestbookDao;
+import guestbook.vo.GuestbookVo;
+
+public class GuestbookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String action = request.getParameter("a");
+		if("add".equals(action)) {
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String contents = request.getParameter("message");
+			
+			GuestbookVo vo = new GuestbookVo();
+			vo.setName(name);
+			vo.setPassword(password);
+			vo.setContents(contents);
+			
+			new GuestbookDao().insert(vo);
+			
+			response.sendRedirect(request.getContextPath()+"/gb");
+			
+		} else if("deleteform".equals(action)) {
+			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/deleteform.jsp");
+			rd.forward(request, response);
+		} else if("delete".equals(action)) {
+			request.setCharacterEncoding("utf-8");
+			
+			Long no = Long.parseLong(request.getParameter("no")); 
+			String password = request.getParameter("password");
+			
+			new GuestbookDao().deleteByNoAndPassword(no, password);
+			
+			response.sendRedirect(request.getContextPath()+"/gb");
+		} else {
+			List<GuestbookVo> list = new GuestbookDao().findAll();
+			
+			request.setAttribute("list", list);
+			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/index.jsp");
+			rd.forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
